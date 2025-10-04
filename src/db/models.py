60e588cef5783e2f0f0fe7210b9a938814ea978a
SavinNik <nikolus5000@gmail.com>
2025-10-04 14:23:52 +0300

    Add DB model for vacancies

---
+++ src/db/models.py
@@ -0,0 +1,20 @@
+from sqlalchemy import Column, Integer, String, DateTime, Float, ARRAY
+from sqlalchemy.ext.declarative import declarative_base
+from datetime import datetime, timezone
+
+Base = declarative_base()
+
+class Vacancy(Base):
+    __tablename__ = "vacancies"
+
+    id = Column(String, primary_key=True)
+    title = Column(String, nullable=False)
+    salary_from = Column(Integer)
+    salary_to = Column(Integer)
+    currency = Column(String, default="RUR")
+    experience = Column(String)
+    experience_label = Column(String)
+    skills = Column(ARRAY(String))
+    area_name = Column(String)
+    published_at = Column(DateTime, default=lambda: datetime.now(timezone.utc))
+    url = Column(String)
